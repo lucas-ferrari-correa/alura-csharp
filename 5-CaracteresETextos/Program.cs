ec70445ef4f3534103d5d8400a746deb772c8447
etaure-lucas-correa <lucas.correa@etaure.com.br>
2021-04-09 09:57:41 -0300

    4 - Variaveis de texto (completo)

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+            Console.WriteLine((int)primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)((int)primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnologia " + 2021;
+            string cursosProgramacao = @" - .NET
+                 - Java
+                 - Javascript";
+                
+            Console.WriteLine(titulo + cursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
