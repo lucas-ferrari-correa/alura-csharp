96b9cb94fcd68f334e9e9d8f2d1dee6749deddc7
Lucas Ferrari Corrêa <Lucas Ferrari Corra@DESKTOP-L38LUII>
2021-04-10 19:38:05 -0300

    Trabalhando com IF (completo)

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João possui mais de 18 anos de idade");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("João posui " + idadeJoao + " anos de idade");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
