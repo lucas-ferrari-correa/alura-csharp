9649dfd6030699da8cdd6b8babcbef884abd36b6
Lucas Ferrari Corrêa <Lucas Ferrari Corra@DESKTOP-L38LUII>
2021-04-12 17:15:23 -0300

    Csharp parte 1 primeiros passos (completo)

---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula Poupanca");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido += valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " mês, você terá R$" + valorInvestido);
+
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido += valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " mês, você terá R$" + valorInvestido);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
